c759d27e5a866f7368a9d53e24db43d0d89343ce
chintio <chintio@sina.com>
2020-09-18 18:13:47 +0800

    注释

--- Source/Urho3D/Graphics/RenderPath.h
+++ Source/Urho3D/Graphics/RenderPath.h
@@ -250,9 +250,14 @@ public:
     Vector<RenderPathCommand> commands_;
 };
 
-/*RenderPath
+// 场景渲染和视口上的任何后处理都由其RenderPath对象定义，该对象既可以从XML文件中读取，也可以通过编程方式创建。
+// 渲染路径由rendertarget定义和命令组成。执行这些命令以生成渲染结果。每个命令要么输出到目标rendertarget& viewport（如果省略输出定义，则默认），要么输出一个命名的rendertargets。MRT（Multiple Render Targets）也可以输出。如果rendertarget是立方体贴图，也可以指定要渲染到的面（0 - 5）。
+// rendertarget的大小可以是绝对大小，也可以是目标视口大小的乘或除。乘数或除数不必是整数。此外，可以将rendertarget声明为“persistent”，以便它不会与相同大小和格式的其他rendertarget混合，并且可以假定其内容也可用于后续帧。
+// 请注意，如果已经在代码中创建了命名的rendertarget纹理，并使用AddManualResource（）将其存储到资源缓存中，则可以直接将其用作输出（通过引用其名称），而无需为其定义rendertarget。
+
+/* 可用命令包括：
 - clear: 清除任何颜色、深度和模板。“颜色清除”可以选择使用远剪裁距离内可见区域的雾颜色。
-- scenepass: 渲染其材质“material technology”包含指定过程的场景对象。将使用状态排序从前到后顺序，或使用无状态排序从后到前顺序。
+- scenepass: 渲染其材质“material technology”包含指定过程（pass）的场景对象。将使用状态排序从前到后顺序，或使用无状态排序从后到前顺序。
             对于延迟渲染，可以选择将对象光照掩码(lightmasks)标记到模板缓冲区。如果顶点光具有必要的着色器组合，则可以在过程(pass)中对其进行处理。
             过程的全局纹理可以绑定到自由纹理单元；这些单元可以是视口、命名的rendertarget或用其路径名标识的纹理资源。
 - quad: 使用指定的着色器渲染一个视口大小的四边形。可以选择指定混合模式（默认值为replace）。
@@ -261,5 +266,45 @@ public:
 - renderui: 将UI渲染到rendertarget中。使用此选项将跳过对backbuffer的默认UI渲染。
 - sendevent: 使用指定的字符串参数（“event name”）发送事件。这可用于在renderpath执行过程中调用自定义代码，通常是自定义低级别渲染。
 */
+
+// Scenepass、quad、forwardlights和lightvolumes命令都允许“命令全局”（command-global ）的着色器编译宏、着色器参数和纹理。例如，在延迟渲染中，lightvolumes命令将绑定G缓冲区纹理以能够计算照明。请注意，当绑定命令全局纹理时，这些纹理（为了优化）只在命令开头绑定一次。如果纹理绑定被对象的材质覆盖，它将“丢失”，直到命令结束。因此，“全局纹理”命令应使用材质不使用的单位。
+// 请注意，renderpath中只存在一个forwardlights或一个lightvolumes命令是合法的。
+// renderpath可以从主XML文件加载（调用Load（）），然后可以通过调用Append（）将其他XML文件（例如每个后处理效果（post-processing effect）对应一个）附加到该文件中。通过调用SetEnabled（）来打开或关闭后处理效果，可以启用或禁用Rendertargets和命令。为了帮助实现这一点，可以通过标记名来标识二者，例如bloom效果对其所有rendertargets和命令使用标记“bloom”。
+// 在同一个命令期间，向目标视口（viewport）写入数据并从中采样（sample）是合法的：将自动生成其内容的乒乓副本（pingpong copies）。如果视口（viewport）启用了“硬件多重采样”，则在采样之前，多采样的backbuffer将被解析为纹理。
+// 有关renderpath定义的示例，请参见bin/CoreData/renderpaths目录中的前向（forward）、延迟（deferred）和灯光预处理（light pre-pass）渲染路径，以及bin/Data/postprocess目录中的后处理renderpath定义。
+
+// 深度模板处理和读取场景深度（Depth-stencil handling and reading scene depth）
+// 通常需要的深度模具表面（depth-stencil surfaces）会在执行渲染路径时自动分配。
+// 特殊的“lineardepth”（同义词“depth”）格式用于在延迟渲染中存储场景深度。它不是实际的硬件深度模具纹理，而是32位单通道（R）浮点rendertarget。（在OpenGL2上它是RGBA，因为所有颜色缓冲区都必须是相同格式的限制。着色器包含文件Samplers.glsl在bin/CoreData/Shaders/GLSL中，提供了将线性深度编码和解码为RGB的函数。）
+// 手动将深度写入rendertarget，同时使用不可读的深度模板表面可确保最佳兼容性，并防止同时使用深度测试和手动深度采样的任何冲突。
+// 还可以定义可读的硬件深度纹理，并指示渲染路径使用它。必须首先使用函数GetReadableDepthSupport（）检查此函数的可用性。在Direct3D9上，这将使用INTZ“hack”格式。若要定义可读深度模具纹理，请使用格式“readabledepth”（同义词“hwdepth”）并通过使用渲染路径命令中的“depthstencil”属性将其设置为深度模具。请注意，必须在每个要使用它的命令中设置它，否则将使用自动分配的深度模具。还请注意，模板通道的存在并不能保证，因此通常由渲染器和视图类（Renderer & View classes）使用的灯光的模板掩码优化将被禁用。
+// 在仅深度渲染过程的特殊情况下，可以直接将可读深度纹理设置为“output”，而根本不需要指定“depthstencil”属性。
+// 填充可读的深度纹理后，可以在任何后续命令中将其绑定到纹理单元。像素着色器应使用ReconstructDepth（）辅助函数从非线性硬件深度值重建0 - 1之间的线性深度值。当可读深度纹理被绑定进行采样时，深度写入将自动禁用，因为修改和采样深度都将是未定义的。
+// 有一个可读硬件深度的渲染路径示例：bin/CoreData/RenderPaths/ForwardHWDepth.xml
+//     渲染路径首先分配与目标视口大小相同的可读深度模具纹理，清除其深度，然后向其渲染仅深度的过程。接下来，目标颜色rendertarget将正常清除，而可读的深度纹理将用作该命令和所有后续命令的深度模板。深度渲染过程之后的任何命令现在都可以将深度纹理绑定到采样单元，例如平滑粒子或SSAO效果。
+//     ForwardDepth.xml“渲染路径”（render path）执行相同的操作，但使用线性深度渲染目标而不是硬件深度纹理。其优点是兼容性更好（保证在不检查GetReadableDepthSupport（）的情况下工作），但性能较差，因为它将执行额外的完整场景渲染过程。
+
+// 软粒子渲染（Soft particles rendering）
+// 软粒子渲染是利用场景深度读取的一个实例。暴露可读深度的默认renderpaths在alpha过程中绑定深度纹理。当包含SOFTPARTICLES着色器编译宏定义时，UnlitParticle&LitParticle着色器将使用此选项。在Bin/CoreData/technologies中名称中包含“Soft”的粒子技术使用此定义。请注意，它们期望一个可读的深度，并且不能与普通的正向renderpath一起工作！
+// 软粒子可以用两种截然不同的方法实现：“收缩”和“膨胀”（"shrinking" and "expanding"）。在“收缩方法”（默认）中，“深度测试”（depth test）处于启用状态，当粒子几何体接近实心几何体时，“软粒子着色器”（soft particle shader）开始降低粒子不透明度。在“膨胀方法”中，粒子应禁用“深度测试”（depth test），当粒子几何体超出实体几何体时，着色器将开始增加粒子不透明度。
+// 有关展开模式，请参见“SoftExpand”粒子技术系列。它们的缺点是，由于不能使用硬件深度测试，性能可能会降低。
+// 最后请注意SoftParticleFadeScale着色器参数，该参数用于控制渐变生效的距离。这在使用软粒子的示例材质中定义(SmokeSoft.xml& LitSmokeSoft.xml)
+
+// 前向光照特殊注意事项（Forward lighting special considerations）
+// 另外，可以指定完全自定义的场景渲染过程，但要记住与前向照明相关的一些事项：
+//     不透明的基过程必须用元数据“base”标记。当正向照明逻辑执行lit base pass优化时，它将搜索前缀为“lit”的过程，即，如果自定义不透明基本过程称为“custombase”，则相应的lit基本过程将为“litcustombase”。
+//     透明基过程必须用元数据“alpha”标记。对于照亮的透明对象，前向照明逻辑将查找前面带有单词“lit”的过程，即如果自定义alpha基过程称为“customalpha”，则相应的照亮过程为“litcustomalpha”。lit drawcalls将与透明的基本过程交错，scenepass命令应该启用从后到前的排序。
+//     如果前向照明和延迟照明混合使用，则G-buffer写入过程必须使用元数据“gbuffer”进行标记，以防止几何体也使用前向灯光进行双重照明。
+//     记住将照明模式（逐顶点/逐像素）标记到定义自定义过程的技术中，因为只有已知的默认过程才能自动猜测照明模式。
+//     如果需要单独的不透明仅环境光基础过程，则forwardlights命令可以选择性地禁用lit base pass优化，而不必接触材质技术。默认情况下启用优化。
+
+// 后处理效果特别注意事项（Post-processing effects special considerations）
+// 后处理效果通常通过使用quad命令来实现。使用与视口rendertarget尺寸不同的中间rendertarget时，通常需要在着色器中引用其（inverse）大小和Direct3D9的半像素偏移。将自动尝试为命名rendertargets指定这些着色器统一值（uniforms）。例如，请查看bloom后处理着色器：因为有一个名为BlurH的rendertarget，renderpath中的每个quad命令都将尝试设置着色器uniforms cBlurHInvSize和cBlurHOffsets（两个Vector2）。请注意，设置着色器uniforms不区分大小写。
+// 在OpenGL后处理着色器中，对采样rendertarget纹理和常规纹理资源的区分是很重要的，因为中间rendertarget（例如G-buffer）可能是垂直反转的。使用GetScreenPos（）或GetQuadtextCoord（）函数从裁剪（clip）坐标获取rendertarget UV坐标；这会自动考虑翻转。要对常规纹理进行采样，请使用GetQuadTexCoordNoFlip（）函数，该函数需要世界坐标而不是裁剪（clip）坐标。
+
+// 多重采样rendertargets（Multisampled rendertargets）
+// Texture2D和TextureCube支持多重采样。在定义维度和格式时，可以通过SetSize（）函数以编程方式启用多重采样。也可以在renderpath的rendertarget定义中设置多重采样。
+// 通常的操作是，多重采样rendertarget将在作为纹理采样之前自动解析为1-sample。这由autoResolve参数表示，其默认值为true。在OpenGL（支持时）和Direct3D11上，还可以通过定义多重采样采样器和使用专用函数（OpenGL上的texelFetch，Direct3D11上的Texture2DMS.Load）访问着色器代码中Texture2D的单个采样。在这种情况下，“autoResolve”参数应该设置为false。请注意，对于立方体纹理或使用Direct3D9时，无法访问单个采样。
+// 通过访问多重采样G-buffer纹理的单个采样，可以实现延迟MSAA渲染器。这有一些性能考虑因素 / 复杂性（当不在三角形边上时，应避免运行每个采样的照明计算），并且默认情况下不会实现。
 
 }
--- Source/Urho3D/Graphics/Shader.h
+++ Source/Urho3D/Graphics/Shader.h
@@ -85,7 +85,7 @@ private:
 // 这些排列的构建是按需进行的：technique和renderpath定义文件都引用了着色器以及与它们一起使用的编译宏定义。此外，引擎将添加与几何体类型和照明相关的内置定义。通常不可能预先枚举可以由单个着色器构建的所有可能的排列。
 // 在Direct3D上，编译的着色器字节码保存到磁盘的“Cache”子目录中（每个编译宏定义组合生成一个着色器字节码文件），以便下次需要着色器排列时跳过可能耗时的编译。在OpenGL上，这种机制是不可用的。
 
-// 内置编译宏定义
+// 内置编译宏定义（compilation defines）
 // 渲染场景对象时，引擎希望针对不同的几何体类型和照明条件存在特定的着色器排列。这些对应于以下编译宏定义：
 // Vertex shader:
 //     NUMVERTEXLIGHTS=1,2,3 or 4:影响对象的顶点灯光数
@@ -101,5 +101,78 @@ private:
 //     SIMPLE_SHADOW, PCF_SHADOW, VSM_SHADOW:要使用的阴影采样质量
 //     SHADOWCMP:使用手动阴影深度比较，Direct3D9仅适用于DF16和DF24阴影贴图格式
 //     HEIGHTFOG:对象区域具有高度雾模式
+// 内置着色器统一值（shader uniforms）
+// 属性是每个顶点位置，表面法线和纹理坐标等都需要的，而统一值则用于为整个图元批次向保持不变的着色器传递数据。对于顶点着色器来说，可能最普遍的统一值就是变换矩阵。
+// 一个统一值在一个图元的绘制过程中是不变的，所以其值不能在glBegin和glEnd之间设置的
+// uniform限定了表示一个变量的值将由应用程序在着色器执行之前指定，并且在图元的处理过程中不会发生变化。
+// uniform变量是由顶点着色器和片段着色器共享的，他们必须声明为全局变量。
+// 当渲染对象或四边形passes时，将设置各种引擎内置统一值以协助渲染。以下是作为HLSL数据类型列出的制服的部分列表。查看Uniforms.glsl获取相应的GLSL Uniforms。
+// Vertex shader uniforms:
+//     float3 cAmbientStartColor:区域环境光渐变的开始颜色值
+//     float3 cAmbientEndColor:区域环境光渐变的结束颜色值
+//     float3 cCameraPos:相机世界坐标
+//     float cNearClip:相机近裁剪距离
+//     float cFarClip:相机远裁剪距离
+//     float cDeltaTime:当前帧的时间步长（帧耗时）
+//     float4 cDepthMode:用于计算0-1之间的线性深度值的参数，以传递到插值器中的像素着色器。
+//     float cElapsedTime:场景的运行时间值。可用于实现材质动画
+//     float4x3 cModel:被渲染对象的世界变换矩阵
+//     float4x3 cView:相机的观察矩阵
+//     float4x3 cViewInv:相机观察矩阵的逆（相机世界变换）
+//     float4x4 cViewProj:相机的观察矩阵x投影矩阵
+//     float4x3 cZone:区域的变换矩阵；用于环境渐变计算
+// Pixel shader uniforms:
+//     float3 cAmbientColor:无环境渐变的区域的环境光颜色
+//     float3 cCameraPosPS:相机世界坐标
+//     float4 cDepthReconstruct:从非线性硬件深度纹理样本重建0-1之间的线性深度值的参数。
+//     float cDeltaTimePS:当前帧的时间步长（帧耗时）
+//     float cElapsedTimePS:场景的运行时间值。
+//     float3 cFogColor:区域的雾颜色
+//     float4 cFogParams:雾计算参数（参见Batch.cpp以及Fog.hlsl获取确切含义）
+//     float cNearClipPS:相机近裁剪距离
+//     float cFarClipPS:相机远裁剪距离
+//     
+
+// 编写着色器
+// 必须分别为HLSL（Direct3D）和GLSL（OpenGL）编写着色器。内置着色器尝试在两种着色器语言上尽可能实现相同的功能。
+// 要编写自己的着色器，可以从最基本的例子着色器开始：Basic, Shadow & Unlit shaders，着色器包含的外部文件通常拥有通用的功能，比如：Uniforms.hlsl, Samplers.hlsl & Transform.hlsl
+// 转换顶点（隐藏了实际的蒙皮、实例或公告牌处理）是一种使用宏和函数组合的小技巧：最安全的方法是逐字复制以下代码：
+//     For HLSL:
+//     float4x3 modelMatrix = iModelMatrix;
+//     float3 worldPos = GetWorldPos(modelMatrix);
+//     oPos = GetClipPos(worldPos);
+//     For GLSL:
+//     mat4 modelMatrix = iModelMatrix;
+//     vec3 worldPos = GetWorldPos(modelMatrix);
+//     gl_Position = GetClipPos(worldPos);
+// 对于Direct3D和OpenGL，顶点和像素着色器被写入同一个文件，并且入口点函数必须是VS（）和PS（）。在OpenGL模式下，其中一个在后台转换为GLSL所需的main（）函数。编译顶点着色器时，编译宏定义“COMPILEVS”始终存在，编译像素着色器时也会出现“COMPILEPS”。这些在着色器包含文件中大量使用，以防止对“错误”类型的着色器无效的构造，并减少编译时间。
+// 顶点着色器输入需要与顶点元素语义匹配才能正确渲染。。在HLSL中，输入的语义在每个着色器中用大写单词（POSITION、NORMAL、TEXCOORD0等）定义，而在GLSL中，默认属性定义在Transform.glsl且通过不区分大小写的字符串“contains”操作与顶点元素语义匹配，并使用可选的数字后缀来定义语义索引。例如，iTexCoord是第一个（语义索引0）纹理坐标，iTexCoord1是第二个纹理坐标（语义索引1）。
+// 统一值（Uniforms）必须以特定方式加前缀，以便引擎能够理解它们：
+//     c表示统一常数，例如cMatDiffColor。当在引擎内部引用时，c被剥离，因此在例如SetShaderParameter（）中称为“MatDiffColor”
+//     s表示纹理采样器，例如sDiffMap。
+// 在GLSL着色器中，将采样器（samplers）指定给正确的纹理单元（texture units）很重要。如果使用的采样器名称不是在引擎中预定义的，例如sDiffMap，只需确保采样器名称中的某个位置有一个数字，它将被解释为纹理单元。例如，地形着色器按以下方式使用纹理单元0-3：
+//     uniform sampler2D sWeightMap0;
+//     uniform sampler2D sDetailMap1;
+//     uniform sampler2D sDetailMap2;
+//     uniform sampler2D sDetailMap3;
+// 硬件蒙皮（hardware skinning）支持的最大骨骼（bones）数取决于图形API，并通过宏定义MAXBONES将其传递到着色器代码。通常，最大值是64，但在Raspberry PI上减少到32，在Direct3D 11和OpenGL 3上增加到128。请参见GetMaxBones（）。
+
+// API差异
+// Direct3D9和Direct3D11共享相同的HLSL着色器代码，同样，OpenGL 2、OpenGL 3、OpenGL ES 2和WebGL共享相同的GLSL代码。宏和一些条件代码用于在可能的情况下隐藏API差异。
+// 为Direct3D11编译HLSL着色器时，宏定义D3D11将会出现，需要遵守以下详细信息：
+//     统一值被组织到常量缓冲区中。查看文件Uniforms.hlsl关于内置的统一值。查看TerrainBlend.hlsl中的示例：定义自己的统一值到“自定义”常量缓冲区插槽。
+//     为每个纹理单元定义纹理和采样器。Samplers.hlsl（Sample2D、SampleCube等）可用于编写在这两个api上工作的代码。它们采用不带“s”前缀的纹理单元名称。
+//     顶点着色器输出位置和像素着色器输出颜色需要使用SV_POSITION和SV_TARGET语义。宏OUTPOSITION和OUTCOLOR0-3可用于在两个api上选择正确的语义。在顶点着色器中，输出位置应最后指定，否则其他输出语义可能无法正常工作。通常，顶点着色器定义的输出语义必须以相同的顺序定义为像素着色器输入。否则，Direct3D着色器编译器可能会指定错误的语义。
+//     在Direct3D11上，必须手动计算剪裁平面坐标。这由CLIPPLANE编译宏定义表示，它由Graphics类自动添加。例如，请参见LitSolid.hlsl着色器。
+//     Direct3D11不支持亮度（luminance）和亮度alpha（luminance-alpha）纹理格式，而是使用R和RG通道。因此，准备在纹理读取中执行适当的旋转。
+//     如果顶点着色器引用顶点缓冲区中不存在的顶点元素，则Direct3D11将无法渲染。
+// 对于OpenGL，当为OpenGL 3+编译GLSL着色器时，宏定义GL3将会出现；对于OpenGL ES 2，宏定义GL_ES会出现；对于WEBGL，宏定义WEBGL将会出现；对于Raspberry Pi，宏定义RPI将会出现。注意以下差异：
+//     在OpenGL 3上，如果着色器源代码没有定义版本，则将使用GLSL版本150。纹理采样函数不同，但通过Samplers.glsl中的宏定义解决. 同样的文件Transform.glsl包含宏以隐藏在声明顶点属性、插值器和片段输出方面的差异。
+//     在OpenGL 3亮度（luminance）中，alpha和luminance alpha纹理格式被弃用，并被R和RG格式取代。因此，准备在纹理读取中执行适当的旋转。
+//     在OpenGL ES 2上需要使用精度限定符。
+
+// 着色器预处理（缓存）
+// 材质技术在不同的照明条件和渲染过程中可能使用的着色器变体会在材质加载时枚举，但由于其数量很大，在用于渲染之前，实际上不会从字节码编译或加载这些着色器变体。尤其是在OpenGL上，在渲染之前编译着色器可能会导致帧速率出现故障。为了避免这种情况，可以将使用过的着色器组合转储到XML文件中，然后进行预加载。请参见图形子系统中的BeginDumpShaders（）、EndDumpShaders（）和PrecacheShaders（）。命令行参数 - ds <file>可用于指示引擎在启动时开始自动转储着色器。
+// 请注意，使用的着色器变体将随图形设置而变化，例如 阴影 质量 采样/PCF/VSM或实例启用/禁用。
 
 }
